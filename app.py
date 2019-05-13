9544ae59f8c6cd6295ba25c156fc79e7b3938207
jelockro <jlockrow@gmail.com>
2019-05-13 11:37:14 -0500

    fixes circular references

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import requests
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+from models import *
 
-from models import Result
 #print(os.environ['APP_SETTINGS'])
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
